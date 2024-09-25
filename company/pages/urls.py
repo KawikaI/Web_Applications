10de8e13175486a7e5d0a9b78af7b7bf6a53f6eb
Kawika Ilac <116589145+KawikaI@users.noreply.github.com>
2024-09-25 11:36:22 -0600

    Delete company directory

--- company/pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import ProductsPageView, home_page_view, AboutPageView
-
-urlpatterns = [
-    path('about/', AboutPageView.as_view(), name='about'),
-    path('', home_page_view, name='home'),
-    path('products/', ProductsPageView.as_view(), name='products'),
-]
